cfa93b4a991993912a3e60166f25c2e9b13f6a54
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-08-15 10:35:22 +0500

    initial commit

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { api } from "@/services";
+
+const initialState = {
+  isAuth: false,
+  isAuthLoading: false,
+};
+
+export const checkAuth = createAsyncThunk("checkAuth", async () => {
+  const response = await api.get("/auth/me");
+  return response.data;
+});
+
+const authSlice = createSlice({
+  name: "Auth",
+  initialState,
+  reducers: {
+    setIsAuth: (state, action) => {
+      state.isAuth = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(checkAuth.pending, (state) => {
+      state.isAuthLoading = true;
+    });
+
+    builder.addCase(checkAuth.fulfilled, (state, action) => {
+      state.isAuthLoading = false;
+      state.isAuth = action.payload.success;
+    });
+  },
+});
+
+export const { setIsAuth } = authSlice.actions;
+export default authSlice.reducer;
