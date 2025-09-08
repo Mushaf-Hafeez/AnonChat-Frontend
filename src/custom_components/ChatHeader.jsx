15edacc7df8542d96507abcaa37dd2789057571c
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-09-08 19:53:59 +0500

    added the exit group UI

--- src/custom_components/ChatHeader.jsx
+++ src/custom_components/ChatHeader.jsx
@@ -1,9 +1,25 @@
 import { setIsSelected, setSelectedGroup } from "@/redux/slices/groupSlice";
-import { X } from "lucide-react";
-import React from "react";
+import { ArrowLeft, EllipsisVertical, LogOut } from "lucide-react";
+import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-toastify";
+
 const ChatHeader = () => {
+  const [iseMenuOpen, setIsMeunOpen] = useState(false);
+  const menuRef = useRef(null);
+
   const { selectedGroup } = useSelector((state) => state.Group);
   const dispatch = useDispatch();
 
@@ -12,16 +28,85 @@ const ChatHeader = () => {
     dispatch(setSelectedGroup(null));
   };
 
+  const handleMenuToggle = (e) => {
+    e.stopPropagation();
+    setIsMeunOpen((prev) => !prev);
+  };
+
+  const handleHeaderClick = (e) => {
+    if (e.target != menuRef.current) {
+      setIsMeunOpen(false);
+    }
+  };
+
+  const handleGroupLeave = () => {
+    dispatch(setIsSelected(false));
+    dispatch(setSelectedGroup(null));
+    toast.success("Group leaved");
+    // Todo: add the group leave functionality
+  };
+
   return (
-    <div className="p-4 flex-center justify-between shadow-xl">
-      <div>
-        <h2 className="text-2xl font-medium">{selectedGroup.groupName}</h2>
-        <p className="text-sm text-neutral-500">
-          {selectedGroup.description.slice(0, 40) + "..."}
-        </p>
+    <div
+      onClick={(e) => handleHeaderClick(e)}
+      className="bg-white p-4 flex-center justify-between shadow-xl rounded-b-xl"
+    >
+      <div className="flex items-center gap-2">
+        <ArrowLeft onClick={handleClick} className="cursor-pointer" size={18} />
+        <div>
+          <h2 className="text-xl font-medium">{selectedGroup.groupName}</h2>
+          <p className="text-sm text-neutral-500">
+            {selectedGroup.description.slice(0, 40) + "..."}
+          </p>
+        </div>
       </div>
 
-      <X onClick={handleClick} className="cursor-pointer" />
+      {/* menu icon */}
+      <div className="relative h-full">
+        <EllipsisVertical
+          size={18}
+          className="cursor-pointer"
+          onClick={handleMenuToggle}
+        />
+        <div
+          ref={menuRef}
+          className={`bg-white border border-neutral-300 absolute z-50 top-3/4 right-0 mt-2 p-2 rounded-lg ${
+            iseMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          <Dialog>
+            <DialogTrigger>
+              <Button
+                variant={"ghost"}
+                className="flex items-center gap-2 cursor-pointer text-neutral-600"
+              >
+                <LogOut size={18} />
+                Exit Group
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Exit group?</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to leave the group.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary">
+                    Close
+                  </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button type="button" onClick={handleGroupLeave}>
+                    Leave
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/SelectedGroupPage.jsx
+++ src/pages/SelectedGroupPage.jsx
@@ -5,10 +5,31 @@ import ChatHeader from "@/custom_components/ChatHeader";
 
 const SelectedGroupPage = () => {
   const { selectedGroup } = useSelector((state) => state.Group);
+  const { user } = useSelector((state) => state.User);
 
   return (
-    <section className="h-full w-full">
+    <section className="bg-neutral-200 h-full w-full">
+      {/* Todo: here in the selected chat page on the right side you show the group information with all its members and remove option if the user is admin */}
       <ChatHeader />
+
+      {/* This part will contain two parts
+      on the left side, there is the container that display all the members of the group to the group admin
+      and on the right side, there is the message for the chat */}
+      <div className="h-[87%] w-full flex-center gap-4 py-4">
+        <div
+          className={`bg-white h-full w-1/2 p-4 rounded-xl shadow-xl overflow-y-auto ${
+            user.role === "CR" || user.role === "GR"
+              ? "hidden lg:block"
+              : "hidden"
+          }`}
+        >
+          here is the list of all the members of the group.
+        </div>
+
+        <div className="bg-white h-full w-full p-4 rounded-xl shadow-xl overflow-y-auto">
+          here is all the messages of the group.
+        </div>
+      </div>
     </section>
   );
 };
