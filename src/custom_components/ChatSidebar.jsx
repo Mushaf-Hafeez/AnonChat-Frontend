87d933ba7f90c31cfb5f1a411a7077675f17ad35
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-05 19:02:37 +0500

    add the logout button for smaller screens

--- src/custom_components/ChatSidebar.jsx
+++ src/custom_components/ChatSidebar.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Logout from "@/custom_components/Logout";
-import { Users } from "lucide-react";
+import { EllipsisVertical, LogOut, Plus, Users } from "lucide-react";
 import { setIsSelected, setSelectedGroup } from "@/redux/slices/groupSlice";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const ChatSidebar = () => {
   const { user } = useSelector((state) => state.User);
@@ -23,7 +40,34 @@ const ChatSidebar = () => {
       } w-full md:w-2/12 h-full bg-white rounded-r-xl col-center justify-between items-start`}
     >
       <div className="h-fit overflow-y-auto w-full p-4">
-        <h2 className="block text-xl font-semibold mb-4">Chat</h2>
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="block text-xl font-semibold">Groups</h2>
+          <div className="flex items-center gap-2">
+            <Plus size={16} className="font-medium cursor-pointer" />
+
+            {/* Dropdown that show the logout button on the smaller screens */}
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="block md:hidden">
+                <button
+                  aria-label="More options"
+                  className="hover:bg-gray-100 p-1 rounded transition-colors"
+                >
+                  <EllipsisVertical size={16} />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  className="p-0"
+                  onSelect={(e) => {
+                    e.preventDefault(); // This prevents the dropdown from closing
+                  }}
+                >
+                  <Logout isChatSidebar={true} />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
 
         {/* group search functionality */}
         {/* Todo: add the search joined groups feature */}
@@ -51,7 +95,7 @@ const ChatSidebar = () => {
       </div>
 
       {/* Logout */}
-      <div className="p-4">
+      <div className="p-4 hidden md:block">
         <Logout isChatSidebar={true} />
       </div>
     </aside>
