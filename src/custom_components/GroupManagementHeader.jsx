1d9d5fbca37c4ee6e0889d95f49ce620fbde17ef
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-14 11:55:23 +0500

    add the group management header

---
+++ src/custom_components/GroupManagementHeader.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { EllipsisVertical, Pencil, Trash2 } from "lucide-react";
+
+const GroupManagementHeader = ({ groupData }) => {
+  return (
+    <div className="flex items-center gap-2">
+      {/* dropdown for smaller screens */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <EllipsisVertical className="block md:hidden" size={16} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className={"flex-col gap-1"}>
+          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+            <Dialog className="w-full">
+              <form>
+                <DialogTrigger className="flex items-center gap-2">
+                  <Pencil />
+                  Edit
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit group</DialogTitle>
+                    <DialogDescription>
+                      {groupData.description}
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <Label htmlFor="name-1">Name</Label>
+                      <Input
+                        id="name-1"
+                        name="name"
+                        defaultValue="Pedro Duarte"
+                      />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="username-1">Username</Label>
+                      <Input
+                        id="username-1"
+                        name="username"
+                        defaultValue="@peduarte"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Save changes</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
+          </DropdownMenuItem>
+          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+            <AlertDialog>
+              <AlertDialogTrigger className="flex items-center gap-2">
+                <Trash2 />
+                Delete
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    group and remove all the group from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel className={"cursor-pointer"}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <AlertDialogAction className={"cursor-pointer"}>
+                    Delete
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* Edit CTA for desktop screens */}
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              className={"hidden md:block cursor-pointer"}
+            >
+              Edit
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit group</DialogTitle>
+              <DialogDescription>{groupData.description}</DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username-1">Username</Label>
+                <Input
+                  id="username-1"
+                  name="username"
+                  defaultValue="@peduarte"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
+
+      {/* Delete dialog for desktop screens */}
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className={"hidden md:block cursor-pointer"}>Delete</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete group
+              and remove all the group from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className={"cursor-pointer"}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction className={"cursor-pointer"}>
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default GroupManagementHeader;
--- src/pages/GroupManagementPage.jsx
+++ src/pages/GroupManagementPage.jsx
@@ -1,8 +1,12 @@
-import Spinner from "@/custom_components/Spinner";
-import { getGroupData } from "@/services/group";
 import React, { useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
+
+import { getGroupData } from "@/services/group";
+
 import LazyLoadingPage from "./LazyLoadingPage";
+
+import GroupManagementHeader from "@/custom_components/GroupManagementHeader";
 
 const GroupManagementPage = () => {
   const [groupData, setGroupData] = useState({});
@@ -32,8 +36,13 @@ const GroupManagementPage = () => {
   }
 
   return (
-    <section className="h-screen w-full bg-neutral-200">
-      <h1>{groupData.groupName}</h1>
+    <section className="h-screen w-full bg-neutral-200 px-4 md:px-10 py-5">
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg md:text-2xl font-medium">Group Management</h2>
+
+        <GroupManagementHeader groupData={groupData} />
+      </div>
     </section>
   );
 };
