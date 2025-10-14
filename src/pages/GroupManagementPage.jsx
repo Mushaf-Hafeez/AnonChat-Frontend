e0caaefd56b231380ab92a8672a02c7a5b2a527f
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-14 10:14:38 +0500

    add group management page

---
+++ src/pages/GroupManagementPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GroupManagementPage = () => {
+  return (
+    <section className="h-screen w-full bg-neutral-200">
+      GroupManagementPage
+    </section>
+  );
+};
+
+export default GroupManagementPage;
