272b734842c0ed86637d43ea854b0809be8428a6
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-18 18:13:49 +0500

    add optional chaining

--- src/pages/GroupAdminProtectedRoute.jsx
+++ src/pages/GroupAdminProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const GroupAdminProtectedRoute = ({ children }) => {
   const { user } = useSelector((state) => state.User);
 
-  if (user.role === "CR" || user.role === "GR") {
+  if (user?.role === "CR" || user?.role === "GR") {
     return children;
   }
 
