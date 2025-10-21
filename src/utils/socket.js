027b41c69992e0a199ef1ff3940f8d0843c7a255
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-21 19:35:14 +0500

    add options in socket configuration

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,4 +2,9 @@ import { io } from "socket.io-client";
 
 const URL = import.meta.env.VITE_BACKEND_URL;
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  withCredentials: true,
+  autoConnect: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
+});
